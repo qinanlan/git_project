6e0f8999a0927a651781255745fe1f8ea14740ce
qinanlan <74592137+qinanlan@users.noreply.github.com>
2020-12-01 17:43:24 +0800

    Update StudentModel.java

--- student_prc/src/com/Student/model/StudentModel.java
+++ student_prc/src/com/Student/model/StudentModel.java
@@ -1,5 +1,14 @@
 package com.Student.model;
 
 public class StudentModel {
+  [branch "master"] 
+	remote = origin 
+	merge = refs/heads/master
+[remote "origin"] 
+	url = https://github.com/huangbangyi2019/git_project.git 
+	fetch = +refs/heads/*:refs/remotes/origin/* 
+	push = refs/heads/master:refs/heads/master
+
+  
 
 }
